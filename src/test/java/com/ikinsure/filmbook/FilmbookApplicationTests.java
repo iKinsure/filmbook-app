a1cfc1812d3e06d1d8ff2394e5498a7555359e6a
iKinsure <wrotekpl@gmail.com>
2021-03-29 15:49:28 +0200

    init commit - spring initializer

---
+++ src/test/java/com/ikinsure/filmbook/FilmbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikinsure.filmbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
