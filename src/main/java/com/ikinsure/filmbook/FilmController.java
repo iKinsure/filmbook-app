fdd180c419a3be5d8b5c877789ca925a24664a16
iKinsure <wrotekpl@gmail.com>
2021-04-01 00:11:41 +0200

    prepared data structure of film entity

---
+++ src/main/java/com/ikinsure/filmbook/FilmController.java
@@ -0,0 +1,26 @@
+package com.ikinsure.filmbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class FilmController {
+
+    private final FilmService service;
+
+    @Autowired
+    public FilmController(FilmService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/ikinsure/filmbook/FilmService.java
@@ -0,0 +1,17 @@
+package com.ikinsure.filmbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilmService {
+
+    private final FilmRepository repository;
+
+    @Autowired
+    public FilmService(FilmRepository repository) {
+        this.repository = repository;
+    }
+
+
+}
