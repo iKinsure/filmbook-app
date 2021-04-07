742c62112719a0b5b25e2eeea42f113ab1c70d80
iKinsure <wrotekpl@gmail.com>
2021-04-07 17:56:24 +0200

    feat: entity 'image' data structure

---
+++ src/main/java/com/ikinsure/filmbook/image/Image.java
@@ -0,0 +1,94 @@
+package com.ikinsure.filmbook.image;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity(name = "Image")
+@Table(name = "image")
+public class Image {
+
+    @Id
+    @SequenceGenerator(name = "image_sequence", sequenceName = "image_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, columnDefinition = "TEXT")
+    private String name;
+
+    @Column(name = "type", nullable = false, columnDefinition = "TEXT")
+    private String type;
+
+    @Lob
+    @Column(name = "data", nullable = false, columnDefinition = "BYTEA")
+    private byte[] data;
+
+    public Image() {
+    }
+
+    public Image(String name, String type, byte[] data) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(id, image.id) && Objects.equals(name, image.name) && Objects.equals(type, image.type) && Arrays.equals(data, image.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, name, type);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", data=" + Arrays.toString(data) +
+                '}';
+    }
+}
